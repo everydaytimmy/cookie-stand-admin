39f863812d8da82f3a5a44059082e43360c6b310
‘Timothy <Timothy.viccari@gmail.com>
2021-07-06 22:24:01 -0400

    restyle

--- pages/index.js
+++ pages/index.js
@@ -22,34 +22,67 @@ export default function Home() {
 
 function CookieStand() {
   return (
-
-    <div className="w-3/4 h-64 mx-auto my-8 bg-green-300 rounded-md">
-      <h1 className="pt-6 text-2xl text-center">Create Cookie Stand</h1>
-      <form className="w-3/4 p-2 mx-auto justiy-center ">
-        <div>
-          <div >
-            <label className="mx-2">Location</label>
-            <input name="location" className="flex-auto w-3/4" />
+    <main className="mx-auto h-96">
+      <div className="mx-auto h-88">
+        <form className="justify-center p-2 mx-10 my-5 bg-green-300 rounded h-168">
+          <div>
+            <h2 className="min-w-full p-2 mx-auto my-2 text-2xl text-center fonr-semibold">
+              Create Cookie Stand
+            </h2>
+            <div className="flex mx-10">
+              <label className="mx-2">Location</label>
+              <input type="text" className="flex-auto" />
+            </div>
+            <div className="flex items-center w-3/4 py-10 mx-20">
+              
+                <div className="justify-items-start">
+                  <label className="py-10">Minimum Customers Per Hour</label>
+                  <input type="text" className="mx-1" />
+                </div>
+                <div className="justify-items-start">
+                  <label className="py-10">Maximum Customers Per Hour</label>
+                  <input type="text" className="mx-1" />
+                </div>
+                <div className="justify-items-start">
+                  <label className="py-10">Average Cookies Per Sale</label>
+                  <input type="text" className="mx-1" />
+                </div>
+              
+              <button type="submit" className="w-48 h-20 mx-auto bg-green-500">Create</button>
+            </div>
           </div>
-          <div className='flex items-center py-10'>
-            <div className="">
-              <label>Minimum Customers Per hour</label>
-              <input name="min" className="" />
-            </div>
-            <div className="">
-              <label>Maximum Customers Per hour</label>
-              <input name="max" className="" />
-            </div>
-            <div className="">
-              <label>Average Cookies per Sale</label>
-              <input name="avg" className="" />
-            </div>
-            <button className="w-64 h-20 px-6 py-2 mx-4 bg-green-500">Create</button>
-          </div>
-        </div>
-      </form>
+          
+        </form>
+      </div>
+    </main>
 
-    </div>
+    // <div className="w-3/4 h-64 mx-auto my-8 bg-green-300 rounded-md">
+    //   <h1 className="pt-6 text-2xl text-center">Create Cookie Stand</h1>
+    //   <form className="w-3/4 p-2 mx-auto justiy-center ">
+    //     <div>
+    //       <div >
+    //         <label className="mx-2">Location</label>
+    //         <input name="location" className="flex-auto w-3/4" />
+    //       </div>
+    //       <div className='flex items-center py-10'>
+    //         <div className="">
+    //           <label>Minimum Customers Per hour</label>
+    //           <input name="min" className="" />
+    //         </div>
+    //         <div className="">
+    //           <label>Maximum Customers Per hour</label>
+    //           <input name="max" className="" />
+    //         </div>
+    //         <div className="">
+    //           <label>Average Cookies per Sale</label>
+    //           <input name="avg" className="" />
+    //         </div>
+    //         <button className="w-64 h-20 px-6 py-2 mx-4 bg-green-500">Create</button>
+    //       </div>
+    //     </div>
+    //   </form>
+
+    // </div>
   )
 }
 
